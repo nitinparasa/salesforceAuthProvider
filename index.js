524d9765d9f673d5232115dbba1e42598d1c66f7
Nitin Parasa <nitin.parasa3@gmail.com>
2018-12-14 16:44:23 +0100

    oAuth - step2 - fix 14

--- index.js
+++ index.js
@@ -10,24 +10,29 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
   
 /* Routing */  
-app.get('/', (req, res) => res.render('pages/index',{ 
+app.get('/', (req, res) => {
+    console.log('enter');
+    res.render('pages/index',{ 
     salesforce_client_id: process.env.CLIENT_ID,
     salesforce_client_secret: process.env.CLIENT_SECRET
-}))
-app.get('/?code=*', () => {
-    const payload = {
-        grant_type: 'authorization_code',
-        code,
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        redirect_uri: 'https://salesforceauthmock.herokuapp.com/' 
-      };
-    fetch('https://login.salesforce.com/services/oauth2/token', {
-        method: "POST",
-        body: payload
-    }).then(function(response) { 
-        console.log(response);
-    })
-});
+})
+})
+// app.all('/', (req,res,next) => {
+//     console.log('entered');
+//     const payload = {
+//         grant_type: 'authorization_code',
+//         code,
+//         client_id: process.env.CLIENT_ID,
+//         client_secret: process.env.CLIENT_SECRET,
+//         redirect_uri: 'https://salesforceauthmock.herokuapp.com/' 
+//       };
+//     fetch('https://login.salesforce.com/services/oauth2/token', {
+//         method: "POST",
+//         body: payload
+//     }).then(function(response) { 
+//         console.log(response);
+//     })
+//     res.next;
+// });
   
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
