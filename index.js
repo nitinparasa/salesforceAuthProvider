c28e2c3f27ba97c7b7b8cab0651967eaa86d7641
Nitin Parasa <nitin.parasa3@gmail.com>
2018-12-17 16:31:25 +0100

    error fix-1

--- index.js
+++ index.js
@@ -19,27 +19,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/success', (req, res) => {
-    const data = req.query.data;
+    //const data = req.query.data;
     res.render('pages/index', {
-        data: data
+        //data: data
     });
 });
-// app.all('/', (req,res,next) => {
-//     console.log('entered');
-//     const payload = {
-//         grant_type: 'authorization_code',
-//         code,
-//         client_id: process.env.CLIENT_ID,
-//         client_secret: process.env.CLIENT_SECRET,
-//         redirect_uri: 'https://salesforceauthmock.herokuapp.com/' 
-//       };
-//     fetch('https://login.salesforce.com/services/oauth2/token', {
-//         method: "POST",
-//         body: payload
-//     }).then(function(response) { 
-//         console.log(response);
-//     })
-//     res.next;
-// });
-  
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
